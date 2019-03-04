fd2d473fa4c43eac25d0510979f77b8f3cf13497
554332076 <554332076@qq.com>
2019-03-04 09:16:51 +0800

    第一次提交

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Admin.layout');
 });
+//类别路由
+Route::get('/cates/create/{id}','Admin\CatesController@create');
+Route::resource('cates','Admin\CatesController');
+//商品路由
+Route::get('/goods','Admin\GoodsController@index');
+Route::resource('goods','Admin\GoodsController');
+//轮播图管理
+Route::resource('lbts','Admin\LbtsController');
+//推荐位管理
+Route::resource('tjws','Admin\TjwController');
+//网站配置管理
+Route::resource('sets','Admin\SetsController');
